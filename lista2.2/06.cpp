e4a16463e4e71827995ffb0ede4b9ba02e384580
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2023-06-02 14:49:35 -0300

    Update 06.cpp

--- lista2.2/06.cpp
+++ lista2.2/06.cpp
@@ -7,6 +7,7 @@ class Cartesiano{
     public:
         void entrada_dados();
         void mostra_dados();
+        void mostra_quadrante();
 };
 
 void Cartesiano::entrada_dados(){
@@ -21,10 +22,25 @@ void Cartesiano::mostra_dados(){
     cout << "y: " << y << endl;
 }
 
+void Cartesiano::mostra_quadrante(){
+    if(x > 0 && y > 0){
+        cout << "1º Quadrante" << endl;
+    }else if(x < 0 && y > 0){
+        cout << "2º Quadrante" << endl;
+    }else if(x < 0 && y < 0){
+        cout << "3º Quadrante" << endl;
+    }else if(x > 0 && y < 0){
+        cout << "4º Quadrante" << endl;
+    }else{
+        cout << "Origem" << endl;
+    }
+}
+
 int main(void){
     Cartesiano dc;
     dc.entrada_dados();
     dc.mostra_dados();
+    dc.mostra_quadrante();
 
     return 0;
 }
